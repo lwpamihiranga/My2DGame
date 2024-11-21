22fa3197dc6c34655bf8bb75706032177deca668
Amith Mihiranga <lwpamihiranga@gmail.com>
2024-11-22 00:18:54 +0530

    Initial commit

---
+++ src/main/GamePanel.java
@@ -0,0 +1,22 @@
+package main;
+
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.Color;
+
+public class GamePanel extends JPanel {
+    final int originalTileSize = 16;
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale;
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol;
+    final int screenHeight = tileSize * maxScreenRow;
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true);
+    }
+}
