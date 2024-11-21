83de117ab5a7d153ee79836487a005d49ae5daf4
Amith Mihiranga <lwpamihiranga@gmail.com>
2024-11-22 01:38:02 +0530

    Draw a rectangle

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,9 +1,10 @@
 package main;
 
 import javax.swing.JPanel;
-import java.awt.Dimension;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class GamePanel extends JPanel implements Runnable {
     final int originalTileSize = 16;
@@ -43,5 +44,10 @@ public class GamePanel extends JPanel implements Runnable {
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setColor(Color.white);
+        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.dispose();
     }
 }
